6ec3aeff598113eefd67050dfcf51ee433607aa7
Ella Skolnick <ellaskolnick@me.com>
2022-10-12 16:41:15 +0100

    additions to tests

--- spec/post_repository_spec.rb
+++ spec/post_repository_spec.rb
@@ -1,6 +1,16 @@
 require 'post_repository'
 
+def reset_posts_table
+  seed_sql = File.read('/Users/ellaskolnick/MakersProjects/databases/resources/seeds/blog_posts_tags.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'blog_posts_tags_test' })
+  connection.exec(seed_sql)
+end
+
 RSpec.describe PostRepository do
+  before(:each) do
+    reset_posts_table
+  end
+
   it "finds posts tagged with 'coding'" do
     repo = PostRepository.new
     tag = repo.find_by_tag('coding')
--- spec/tag_repository_spec.rb
+++ spec/tag_repository_spec.rb
@@ -1,6 +1,16 @@
 require 'tag_repository'
 
+def reset_tags_table
+  seed_sql = File.read('/Users/ellaskolnick/MakersProjects/databases/resources/seeds/blog_posts_tags.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'blog_posts_tags_test' })
+  connection.exec(seed_sql)
+end
+
 RSpec.describe TagRepository do
+  before(:each) do
+    reset_tags_table
+  end
+
   it "finds tags attached to first post" do
     repo = TagRepository.new
     post = repo.find_by_post(1)
